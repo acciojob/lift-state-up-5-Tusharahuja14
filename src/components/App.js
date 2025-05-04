7a2691dfcfe5e0ca8c053744ad290d5e183bf45f
Tushar Ahuja <tusharahuja668@gmail.com>
2025-05-04 15:48:38 +0530

    change in repo

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,45 @@
-
 import React, { useState } from "react";
-import './../styles/App.css';
-import LoginForm from "./LoginForm";
+import "./../styles/App.css";
 
 const App = () => {
-  const[isLoggedIn,setIsLoggedIn]=useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // For test, no actual validation is needed
+    setIsLoggedIn(true);
+  };
+
   return (
     <div>
-        {/* Do not remove the main div */}
-        {isLoggedIn?
-        <h1>Parent Component</h1>:
-        <LoginForm setIsLoggedIn={setIsLoggedIn}/>
-        }
-
+      {/* Do not remove the main div */}
+      {isLoggedIn ? (
+        <h1>Parent Component</h1>
+      ) : (
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="name">Name:</label>
+          <input
+            type="text"
+            id="name"
+            onChange={(e) => setName(e.target.value)}
+          />
+          <br />
+          <br />
+          <label htmlFor="password">Password:</label>
+          <input
+            type="password"
+            id="password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <br />
+          <br />
+          <button type="submit">Login</button>
+        </form>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
